5c40807aa77831c36da8c0f6c9d639972b199ce6
Carlos Costa <carllos.nc@gmail.com>
2025-02-25 23:56:28 -0300

    feat: extend db-types to include Link type and UpdatePage structure

--- types/db-types.ts
+++ types/db-types.ts
@@ -1,4 +1,13 @@
-import { pages } from "@/data/db-schemas/page-schema"
+import { pages } from '@/data/db-schemas/page-schema'
+import { links } from '@/data/db-schemas/link-schema'
 
 export type Page = typeof pages.$inferSelect
 export type NewPage = typeof pages.$inferInsert
+export type Link = typeof links.$inferSelect
+export type NewLink = typeof links.$inferInsert
+export type UpdatePage = {
+  id: string
+  title: string
+  description: string | null
+  slug: string
+}
