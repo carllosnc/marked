51e3e2c3619e8eca3231e749c21da5edd0da986e
Carlos Costa <carllos.nc@gmail.com>
2025-03-01 02:33:56 -0300

    fix: replace UserButton with ClerkButton in HomeHeader and simplify navigation

--- components/home/home-header.tsx
+++ components/home/home-header.tsx
@@ -1,8 +1,8 @@
-import { UserButton } from '@/components/user-button'
 import { LogoHorizontal } from '../logo'
 import { Button } from '@/components/ui/button'
 import { NavLink } from 'react-router'
 import { useUser } from '@clerk/react-router'
+import { ClerkButton } from '../clerk-button'
 
 export function HomeHeader() {
   const { user, isLoaded } = useUser()
@@ -13,14 +13,12 @@ export function HomeHeader() {
         <LogoHorizontal className="w-[120px] h-auto fill-neutral-950 dark:fill-white" />
 
         <div className="flex gap-[20px]">
-          {isLoaded && user?.firstName && (
-            <NavLink to="/dashboard">
-              <Button size="sm" variant="outline">
-                Dashboard
-              </Button>
-            </NavLink>
-          )}
-          <UserButton />
+          <NavLink to="/dashboard">
+            <Button size="sm" variant="outline">
+              Dashboard
+            </Button>
+          </NavLink>
+          <ClerkButton />
         </div>
       </div>
     </nav>
