27dade06500aff437226ac62e7db09045f98fd99
Carlos Costa <carllos.nc@gmail.com>
2025-03-01 02:33:51 -0300

    fix: replace UserButton with ClerkButton and add navigation links in DashboardHeader

--- components/dashboard/dashboard-header.tsx
+++ components/dashboard/dashboard-header.tsx
@@ -1,5 +1,7 @@
-import { UserButton } from "@/components/user-button";
-import { LogoHorizontal } from "@/components/logo";
+import { LogoHorizontal } from '@/components/logo'
+import { ClerkButton } from '../clerk-button'
+import { NavLink } from 'react-router'
+import { Button } from '../ui/button'
 
 export function DashboardHeader() {
   return (
@@ -7,7 +9,14 @@ export function DashboardHeader() {
       <div className="w-full m-auto flex justify-between items-center max-w-[900px] h-[70px]">
         <LogoHorizontal className="w-[120px] h-auto fill-neutral-950 dark:fill-white" />
 
-        <UserButton />
+        <div className="flex gap-[20px]">
+          <NavLink to="/">
+            <Button size="sm" variant="outline">
+              Home
+            </Button>
+          </NavLink>
+          <ClerkButton />
+        </div>
       </div>
     </nav>
   )
