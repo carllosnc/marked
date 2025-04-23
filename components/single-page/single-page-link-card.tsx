b60e9433bccda43d44ef4534fe773765dbaefaaf
Carlos Costa <carllos.nc@gmail.com>
2025-04-22 23:44:40 -0300

    fix: add formatted creation date display in SinglePageLinkCard component

--- components/single-page/single-page-link-card.tsx
+++ components/single-page/single-page-link-card.tsx
@@ -4,6 +4,7 @@ import { Spinner } from '@/components/ui/spinner'
 import { Trash } from 'lucide-react'
 import { toast } from 'sonner'
 import { useDeleteLink } from '@/data/db-hooks/links-hooks'
+import { formatDate } from '@/lib/utils'
 
 type props = {
   link: Link
@@ -29,6 +30,8 @@ export function SinglePageLinkCard({ link }: props) {
     <article className="card-block card-linkable flex-col gap-[20px] items-start overflow-hidden flex md:items-center md:flex-row">
       <a className="w-full" href={link.url!} target="_blank" rel="noreferrer">
         <div className="flex flex-col gap-[10px]">
+          <small className="text-color">{formatDate(link.created_at!)}</small>
+
           <span className="link-color text-[14px] truncate max-w-[600px]">
             {link.url!}
           </span>
