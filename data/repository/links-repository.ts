5b7d6b02ab6af8123ffeb808a6042677eeb9180a
Carlos Costa <carllos.nc@gmail.com>
2025-04-22 23:44:48 -0300

    fix: remove ascending order from getLinks function and reverse results

--- data/repository/links-repository.ts
+++ data/repository/links-repository.ts
@@ -1,18 +1,16 @@
 import { db } from '@/app/database'
 import type { NewLink } from '@/types/db-types'
 import { links } from '@/data/db-schemas/link-schema'
-import { eq, asc } from 'drizzle-orm'
+import { eq } from 'drizzle-orm'
 
 export async function createLink(link: NewLink) {
   return await db.insert(links).values(link).returning()
 }
 
 export async function getLinks(pageId: string) {
-  return await db
-    .select()
-    .from(links)
-    .where(eq(links.page_id, pageId))
-    .orderBy(asc(links.created_at))
+  return (
+    await db.select().from(links).where(eq(links.page_id, pageId))
+  ).reverse()
 }
 
 export async function deleteLink(id: string) {
